7fd48d533b2706cac6039a33ae1008dc03d96c40
Mohammed Yaseen <mohammedysn15@gmail.com>
2022-06-21 02:07:15 +0100

    Implement product tracking and management

--- product_catalogue.py
+++ product_catalogue.py
@@ -1,13 +1,22 @@
 class ProductCatalogue:
-    def __init__(self, *products):
+    def __init__(self, *prods):
         # dict product code -> product obj
         self.catalogue = {}
-        for product in products:
-            self.catalogue[product.product_code] = product
+        for prod in prods:
+            self.catalogue[prod.code] = prod
 
-    def sample_all_products(self):
+    def sample_all_prods(self):
         for prod in self.catalogue.values():
             prod.sample_current_price()
+
+    def sample_prod(self, prod_code):
+        self.catalogue[prod_code].sample_current_price()
+
+    def remove_prod(self, prod_code):
+        self.catalogue.pop(prod_code)
+
+    def get_all_prods(self):
+        return self.catalogue.values()
 
     def __str__(self):
         msg = ""
--- run.py
+++ run.py
@@ -1,7 +1,9 @@
-from file_manager import FileManager
 from product import Product
 from product_catalogue import ProductCatalogue
 
 if __name__ == '__main__':
-    catalogue = ProductCatalogue(Product('174948842042'), Product('313914351201'))
-    catalogue.sample_all_products()
+    catalogue = ProductCatalogue(Product('174948842042'), Product('265163604233'), Product('hello'))
+    catalogue.sample_all_prods()
+    print(catalogue)
+    for prod in catalogue.get_all_prods():
+        print(prod.get_link())
