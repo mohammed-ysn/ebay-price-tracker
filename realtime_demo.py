6500fc64dd438aacf666a8e80d9f2717cc55f392
Mohammed Yaseen <mohammedysn15@gmail.com>
2023-10-03 18:37:44 +0100

    Update realtime demo

--- realtime_demo.py
+++ realtime_demo.py
@@ -1,14 +1,17 @@
 from time import sleep
 
-from product_data.ebay_product import EBayProduct
-from product_data.product_catalogue import ProductCatalogue
-from visuals.graph import plot_price
+from product.ebay_product import EBayProduct
+from product.catalogue import ProductCatalogue
 
 if __name__ == "__main__":
-    demo_cat = ProductCatalogue(EBayProduct("Gym Bottle", "265163604233"))
-    demo_cat.sample_all_prods()
+    product = EBayProduct("Demo Product", input("Enter product ID: "))
+    catalogue = ProductCatalogue(product)
+    print("Sampling product price...")
+    catalogue.sample_all_prods()
     sleep(1)
-    demo_cat.sample_all_prods()
+    print("Sampling product price again...")
+    catalogue.sample_all_prods()
     sleep(1)
-    demo_cat.sample_all_prods()
-    plot_price(demo_cat.get_prod("265163604233"))
+    print("Sampling product price one last time...")
+    catalogue.sample_all_prods()
+    product.plot_price()
