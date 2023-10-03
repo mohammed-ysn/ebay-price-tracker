f83b5142c066bc44b7dcd8e6a077d4b2acd05a79
Mohammed Yaseen <mohammedysn15@gmail.com>
2023-10-03 18:16:45 +0100

    Move plot method to product

--- prior_demo.py
+++ prior_demo.py
@@ -1,9 +1,11 @@
 from datetime import datetime
+import os
 
 from utils import file_manager
 from product.ebay_product import EBayProduct
 from product.catalogue import ProductCatalogue
-from visuals.graph import plot_price
+
+DATA_DIR = os.path.join(".", "data")
 
 
 def create_demo():
@@ -13,11 +15,14 @@ def create_demo():
     demo_cat.catalogue["012345"].price_history[datetime(2022, 6, 15, 12, 13)] = 56.75
     demo_cat.catalogue["012345"].price_history[datetime(2022, 6, 15, 18, 23)] = 55
     demo_cat.catalogue["012345"].price_history[datetime(2022, 6, 15, 22, 6)] = 55
-    file_manager.dump("./data/demo_catalogue", demo_cat)
-    print("Created ./data/demo_catalogue")
+
+    file_manager.dump(os.path.join(DATA_DIR, "demo_catalogue"), demo_cat)
+    print(f"Created {os.path.join(DATA_DIR, 'demo_catalogue')}")
 
 
 if __name__ == "__main__":
     create_demo()
-    catalogue = file_manager.load("./data/demo_catalogue")
-    plot_price(catalogue.get("012345"))
+
+    catalogue = file_manager.load(os.path.join(DATA_DIR, "demo_catalogue"))
+    product = catalogue.get("012345")
+    product.plot_price()
--- product/product.py
+++ product/product.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 from datetime import datetime
 from typing import Dict, Optional
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 class Product(ABC):
@@ -46,6 +48,47 @@ class Product(ABC):
             return self.price_history[max(self.price_history)]
         else:
             return None
+
+    def plot_price(self) -> None:
+        timestamps = np.fromiter(self.price_history.keys(), dtype="datetime64[s]")
+        prices = np.fromiter(self.price_history.values(), dtype="float")
+
+        # Sort data by timestamps
+        sorted_indices = np.argsort(timestamps)
+        timestamps = timestamps[sorted_indices]
+        prices = prices[sorted_indices]
+
+        fig, ax = plt.subplots()
+
+        # Plot max and min prices if they are different
+        min_price = np.min(prices)
+        max_price = np.max(prices)
+        if min_price != max_price:
+            ax.axhline(y=min_price, color="green", linestyle="dashed", alpha=0.7)
+            ax.axhline(y=max_price, color="red", linestyle="dashed", alpha=0.7)
+            ax.text(timestamps[0], min_price, f"£{min_price:.2f}")
+            ax.text(timestamps[0], max_price, f"£{max_price:.2f}")
+
+        # Plot price step graph
+        ax.step(timestamps, prices, where="post")
+
+        ax.scatter(timestamps, prices, marker="x", color="black", alpha=0.7)
+
+        ax.set_xlabel("Time")
+        ax.set_ylabel("Price")
+
+        # Use currency formatting for y-axis
+        ax.yaxis.set_major_formatter("£{x:1.2f}")
+
+        ax.grid(alpha=0.3)
+
+        fig.canvas.manager.set_window_title(f"{self.name} Price History")
+        fig.suptitle(f"{self.name} Price History")
+
+        # Rotate x-axis labels for readability
+        plt.xticks(rotation=17)
+
+        plt.show()
 
     def __str__(self):
         return f"{self.name} @ £{self.most_recent_price()}"
